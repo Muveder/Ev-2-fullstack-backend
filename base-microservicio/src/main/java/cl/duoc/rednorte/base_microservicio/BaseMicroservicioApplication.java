e56e245ce4b2aa6c537b387429b56a9f1833d60d
Muveder <er.aubel@duocuc.cl>
2026-05-11 22:26:28 -0400

    Clean Architecture: Proyecto optimizado sin archivos pesados

---
+++ base-microservicio/src/main/java/cl/duoc/rednorte/base_microservicio/BaseMicroservicioApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.rednorte.base_microservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseMicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseMicroservicioApplication.class, args);
+	}
+
+}
---
+++ base-microservicio/src/test/java/cl/duoc/rednorte/base_microservicio/BaseMicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.rednorte.base_microservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseMicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
